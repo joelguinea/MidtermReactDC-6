b6cbefbc0192eefd0a21aac2595e9d739f03f288
joelguinea <joelguinea69@gmail.com>
2022-10-16 10:16:52 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./components/Navbar";
+import {
+  Venues,
+  Dashboard,
+  About,
+  ErrorPage,
+} from "./pages";
+import VenueSinglePage from "./view/VenueSinglePage";
+
+function App() {
+  return (
+    <>
+    
+    <BrowserRouter>
+        <Sidebar>
+        <Routes>
+          <Route path="/venues" element={<Venues />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          
+
+          <Route path="/venues/:id" element={<VenueSinglePage />} />
+
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+        </Sidebar>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="h1 bg-warning text-dark text-center">
+      Page not Found 404
+    </div>
+  );
+};
+
+export default ErrorPage;
